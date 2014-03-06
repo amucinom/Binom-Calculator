77ea82ab27f0ffb7f5a05e7e0d6503e40207354b
Aldo Mucino-Martinez <aldomucinomartinez@gmail.com>
2014-03-05 18:04:30 -0700

    Updated layout, fixed calculation bugs

--- scripts/binom.js
+++ scripts/binom.js
@@ -6,10 +6,20 @@ function binomDistro() {
 	var secParam = document.binomForm.secParam.value;
 
 	var meanX = n*p;
+	document.binomForm.meanX.value = meanX.toFixed(4);
+
 	var stdDev = Math.sqrt(n*p*(1 - p));
+	document.binomForm.stdDev.value = stdDev.toFixed(4);
+
 	var pHat = ((n*p) / n);
+	document.binomForm.pHat.value = pHat.toFixed(4);
+
 	var normSD = Math.sqrt((p*(1 - p)))/n;
+	document.binomForm.normSD.value = normSD.toFixed(4);
+
 	var probZPos = (secParam - p)/(Math.sqrt((p*(1 - p))/n));
-	var probZNeg = (firstParam - p)/(Math.sqrt((p*(1 - p))/n));
-	
+	document.binomForm.probZPos.value = probZPos.toFixed(4);
+
+	var probZNeg = (firstParam - p)/(Math.sqrt((p*(1 - p))/n));	
+	document.binomForm.probZNeg.value = probZNeg.toFixed(4);
 }
